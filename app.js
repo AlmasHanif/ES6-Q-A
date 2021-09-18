537a314706becc00ed20538ad8110ea49ec99730
Almas Hanif <almashanif126@gmail.com>
2021-09-18 17:26:29 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,83 @@
+/// Q no 1 ///
+
+//write a function which returns array. call the function and destructure array values and print the value
+
+
+
+// function destructuring() {
+//     return ["karachi" , "lahore" , "islamabad" , "quetta"];
+//   }
+  
+//   let a, b , c , d;
+//   [a, b , c , d] = destructuring();
+//   console.log(a); 
+//   console.log(b);
+//   console.log(c);
+//   console.log(d);
+
+//   document.write(`${a}   ${b}   ${c} ${d}`)
+
+
+  ////Q no 2////
+
+  // Make a car object with model, year, color. display all the values by destructuring
+
+
+//   let car = {
+//       model : "swift" ,
+//       colour : "red" ,
+//       year : 2021 
+//   }
+
+//   let { model , colour , year} = car;
+//   console.log(model)
+//   console.log(colour)
+//   console.log(year)
+
+
+////Q no 3////
+
+// use three variable name, greeting and display value using template literals
+
+// let name1 = "sara";
+// let name2 = "hina";
+// let name3 = "aisha";
+
+// console.log(`Hello ${name1} 
+//  Hello ${name2}
+//  Hello ${name3}`)
+
+
+
+////Q no 4////
+
+// use array [1,2,3,4,5,6,7,8,9,10] assign first three values in seperate variables and all remaining in one element as an array
+
+
+
+// const numb = [1,2,3,4,5,6,7,8,9,10];
+
+// const [num1 , num2 , num3 , ...rest] = numb
+
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+// console.log(rest)
+
+
+
+////Q no 5////
+
+// if car speed is less than 80 display "car speed is slow", if greater than 80 but less than 120 than display 
+// " car speed is moderate" and if greater than 120 display "car speed is fast"
+
+
+// let carSpeed = prompt("enter your car speed");
+
+// let check ;
+
+// check = ( carSpeed <= 80) ? "car speed is slow" : (carSpeed > 80 && carSpeed < 120) ? " car speed is moderate" :( carSpeed > 120 )? "car speed is fast" : "enter your car speed"
+// alert(check)
+
+
+
